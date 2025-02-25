5ac546f8d9f63f2c463be16b15d0b85a37381f47
lukzthebest88 <lukasplol12@gmail.com>
2025-02-25 19:10:16 +0100

    UserManagement

---
+++ Mutternboard/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Mutterboard.Models;
+
+namespace Mutternboard.Controllers
+{
+    [Authorize(Roles = "Admin")] // Nur für Admins zugänglich
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            var userRoles = new Dictionary<string, string>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                userRoles[user.Id] = roles.FirstOrDefault() ?? "User"; // Falls keine Rolle, Standard auf "User"
+            }
+
+            ViewBag.UserRoles = userRoles; // Rollen an View übergeben
+            return View(users);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction(nameof(Index)); // Zurück zur Liste
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SetAdmin(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+                await _userManager.RemoveFromRoleAsync(user, "User");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveAdmin(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                await _userManager.RemoveFromRoleAsync(user, "Admin");
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
